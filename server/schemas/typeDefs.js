49790a43b40b332b10f05975272c09cf4f4b7f52
Salena O'Neill <salenaoneill@salenas-air.lan>
2023-03-29 09:51:14 -0500

    resolvers & typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gpl } = require('apollo-server-express');
+
+const typeDefs = gpl`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
+    
+    input savedBook {
+        description: String!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        Link: String
+    }
+
+    typeAuth {
+        token: ID!
+        user: User
+    }
+
+    typeQuery {
+        me: User
+    }
+    
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: savedBook!): User
+        removeBook(bookId: ID!): User
+    }`;
+
+    module.exports = typeDefs;
